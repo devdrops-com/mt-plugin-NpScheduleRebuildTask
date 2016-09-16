58853ca2d86ed0567d1d4bc20ea7b35cea0e2c31
shin@nawagate <shin@nawagate.com>
2016-09-16 14:50:25 +0900

    開発中

--- NpScheduleRebuildTask/plugins/NpScheduleRebuildTask/lib/NpScheduleRebuildTask/Tasks.pm
+++ NpScheduleRebuildTask/plugins/NpScheduleRebuildTask/lib/NpScheduleRebuildTask/Tasks.pm
@@ -1,22 +1,50 @@
 package NpScheduleRebuildTask::Tasks;
 use strict;
-
+use Data::Dumper;
 sub do_task {
 doLog("do_task");
-  my ($ctx, $args) = @_;
-  my $blog_id = $ctx->stash('blog_id');
-doLog("blog_id => " . $blog_id);
   my $plugin = MT->component('NpScheduleRebuildTask');
-  my @blog_config_index_template_ids = split(/,/, $plugin->get_config_value('blog_config_index_template_ids', $blog_id));
-doLog("$blog_config_index_template_ids => " . $blog_config_index_template_ids);
+  require MT::Website;
+  my @websites = MT::Website->load();
+  foreach my $website (@websites) {
+doLog(Dumper($plugin));
+doLog("website_id => " . $website->id);
+    # my $blog_config_index_template_ids = $plugin->get_config_value('blog_config_index_template_ids', "blog:$website->id") || "";
+    my $blog_id = 'blog:' . $website->id;
+    my $blog_config_index_template_ids = $plugin->get_config_value('blog_config_index_template_ids', $blog_id) || "";
+doLog("blog_config_index_template_ids => " . $blog_config_index_template_ids);
+#     my $word_sys = $plugin->get_config_value('blog_config_index_template_ids', 'system');
+# doLog("word_sys => " . $word_sys);
+# MT->rebuild_indexes(
+#   BlogID   => $website->id,
+#   Template => MT::Template->load( 35 ),
+#   Force    => 1,
+# );
+    my @blog_config_index_template_ids = split(/,/, $blog_config_index_template_ids);
   foreach my $blog_config_index_template_id (@blog_config_index_template_ids){
-    next unless $$blog_config_index_template_id;
+doLog("blog_config_index_template_id => " . $blog_config_index_template_id);
+      next unless $blog_config_index_template_id;
     MT->rebuild_indexes(
-      BlogID   => $blog_id,
-      Template => MT::Template->load( $blog_config_index_template_id ),
-      Force    => 1,
+        BlogID   => $website->id,
+        # Template => MT::Template->load( $blog_config_index_template_id ),
+        # Force    => 1,
     );
   }
+}
+#   require MT::Blog;
+#   my @blogs = MT::Blog->load();
+#   foreach my $blog (@blogs) {
+#     my @blog_config_index_template_ids = split(/,/, $plugin->get_config_value('blog_config_index_template_ids', $blog->id));
+#     foreach my $blog_config_index_template_id (@blog_config_index_template_ids){
+# doLog("blog_config_index_template_id => " . $blog_config_index_template_id);
+#       next unless $blog_config_index_template_id;
+#       MT->rebuild_indexes(
+#         BlogID   => $blog->id,
+#         Template => MT::Template->load( $blog_config_index_template_id ),
+#         Force    => 1,
+#       );
+#     }
+#   }
 }
 
 sub doLog {
